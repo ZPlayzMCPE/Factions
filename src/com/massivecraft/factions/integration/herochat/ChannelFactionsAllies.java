9647d520783c9753cf22487437b3265dc0afa973
ZPlayzMCPE <jameshayward3800@gmail.com>
2017-10-24 16:35:03 +0100

    Update ChannelFactionsAllies.java

--- src/com/massivecraft/factions/integration/herochat/ChannelFactionsAllies.java
+++ src/com/massivecraft/factions/integration/herochat/ChannelFactionsAllies.java
@@ -1,4 +1,4 @@
-package com.massivecraft.factions.integration.herochat;
+package com.massivecraft.factions.integration.PexChat;
 
 import com.massivecraft.factions.Rel;
 import com.massivecraft.factions.entity.MConf;
@@ -12,30 +12,30 @@ public class ChannelFactionsAllies extends ChannelFactionsAbstract
 	public static final Set<Rel> targetRelations = EnumSet.of(Rel.MEMBER, Rel.RECRUIT, Rel.ALLY);
 	@Override public Set<Rel> getTargetRelations() { return targetRelations; }
 	
-	@Override public String getName() { return MConf.get().herochatAlliesName; }
+	@Override public String getName() { return MConf.get().PexChatAlliesName; }
 	
-	@Override public String getNick() { return MConf.get().herochatAlliesNick; }
-	@Override public void setNick(String nick) { MConf.get().herochatAlliesNick = nick; }
+	@Override public String getNick() { return MConf.get().PexChatAlliesNick; }
+	@Override public void setNick(String nick) { MConf.get().PexChatAlliesNick = nick; }
 	
-	@Override public String getFormat() { return MConf.get().herochatAlliesFormat; }
-	@Override public void setFormat(String format) { MConf.get().herochatAlliesFormat = format; }
+	@Override public String getFormat() { return MConf.get().PexChatAlliesFormat; }
+	@Override public void setFormat(String format) { MConf.get().PexChatAlliesFormat = format; }
 	
-	@Override public ChatColor getColor() { return MConf.get().herochatAlliesColor; }
-	@Override public void setColor(ChatColor color) { MConf.get().herochatAlliesColor = color; }
+	@Override public ChatColor getColor() { return MConf.get().PexChatAlliesColor; }
+	@Override public void setColor(ChatColor color) { MConf.get().PexChatAlliesColor = color; }
 	
-	@Override public int getDistance() { return MConf.get().herochatAlliesDistance; }
-	@Override public void setDistance(int distance) { MConf.get().herochatAlliesDistance = distance; }
+	@Override public int getDistance() { return MConf.get().PexChatAlliesDistance; }
+	@Override public void setDistance(int distance) { MConf.get().PexChatAlliesDistance = distance; }
 	
-	@Override public void addWorld(String world) { MConf.get().herochatAlliesWorlds.add(world); }
-	@Override public Set<String> getWorlds() { return MConf.get().herochatAlliesWorlds; }
-	@Override public void setWorlds(Set<String> worlds) { MConf.get().herochatAlliesWorlds = worlds; }
+	@Override public void addWorld(String world) { MConf.get().PexChatAlliesWorlds.add(world); }
+	@Override public Set<String> getWorlds() { return MConf.get().PexChatAlliesWorlds; }
+	@Override public void setWorlds(Set<String> worlds) { MConf.get().PexChatAlliesWorlds = worlds; }
 	
-	@Override public boolean isShortcutAllowed() { return MConf.get().herochatAlliesIsShortcutAllowed; }
-	@Override public void setShortcutAllowed(boolean shortcutAllowed) { MConf.get().herochatAlliesIsShortcutAllowed = shortcutAllowed; }
+	@Override public boolean isShortcutAllowed() { return MConf.get().PexChatAlliesIsShortcutAllowed; }
+	@Override public void setShortcutAllowed(boolean shortcutAllowed) { MConf.get().PexChatAlliesIsShortcutAllowed = shortcutAllowed; }
 	
-	@Override public boolean isCrossWorld() { return MConf.get().herochatAlliesCrossWorld; }
-	@Override public void setCrossWorld(boolean crossWorld) { MConf.get().herochatAlliesCrossWorld = crossWorld; }
+	@Override public boolean isCrossWorld() { return MConf.get().PexChatAlliesCrossWorld; }
+	@Override public void setCrossWorld(boolean crossWorld) { MConf.get().PexChatAlliesCrossWorld = crossWorld; }
 	
-	@Override public boolean isMuted() { return MConf.get().herochatAlliesMuted; }
-	@Override public void setMuted(boolean value) { MConf.get().herochatAlliesMuted = value; }
+	@Override public boolean isMuted() { return MConf.get().PexChatAlliesMuted; }
+	@Override public void setMuted(boolean value) { MConf.get().PexChatAlliesMuted = value; }
 }
